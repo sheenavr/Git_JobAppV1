fc2d1b7cba48cc94f22967162ccf212d06670509
sheenavr <vrsheenavr@gmail.com>
2024-02-18 01:11:44 -0500

    First commit

---
+++ src/main/java/com/job/application/GitJobAppApplication.java
@@ -0,0 +1,13 @@
+package com.job.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitJobAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitJobAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/job/application/controller/JobAppController.java
@@ -0,0 +1,26 @@
+package com.job.application.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.job.application.model.Job;
+@RestController
+@RequestMapping("/api/v1/jobs")
+public class JobAppController {
+	private List<Job> jobs=new ArrayList<>();
+	@GetMapping("/allJobs")
+	public List<Job> findAll(){
+		return jobs;
+	}
+	@PostMapping("/addJob")
+	public String addJob(@RequestBody Job job) {
+		jobs.add(job);
+		return "Jod Details Added successfully";
+	}
+
+}
